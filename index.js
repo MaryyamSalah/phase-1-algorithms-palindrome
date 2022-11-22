9fd7cf7914c765e39b9bd88c97296d836b4aee31
MaryyamSalah <maryyamslah20@gmail.com>
2022-11-22 15:13:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
+  const strArr = str.split("")
+  const reversedrArr = str.split("").reverse()
+  let a = 0
+  for (let i in reversedrArr){
+    
+    if (reversedrArr[i] === strArr[i]){
+       a++;
+    }
+  }
+  if (a===strArr.length){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 /* 
@@ -7,14 +22,21 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+-splitting the string
+-reversing the string
+-taking each letter by for loop
+-if condition for comparing each character
+-incraese a
+-if the character is equal to the lenghth return true 
+-otherwise return false
+ 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("lal"));
 
   console.log("");
 
